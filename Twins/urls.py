7e81ad6f33b407eb6037f7e661e3bdc713d32832
Churchillk <132873688+Churchillk@users.noreply.github.com>
2024-11-20 19:39:37 +0300

    fixed login

--- Twins/urls.py
+++ Twins/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('store/', include('TwinsAdminUsers.urls')),
     path("", include("Home.urls")),
     path("", include("Authentications.urls")),
 ]
